4a59ad97be5473b2d96886d9bfa95f3801af05c1
MigenaPl <141563111+MigenaPl@users.noreply.github.com>
2024-06-22 22:14:31 +0200

    api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    // documentation: https://www.shecodes.io/learn/apis/dictionary
+    let apiKey = `9da03fffet062oac34bd00790f54faa4`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
